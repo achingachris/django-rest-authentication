e61d8f157a00afa757adec614be170a0cbe49a2b
ChrisAchinga <achinga.chris@gmail.com>
2021-01-03 11:45:30 +0300

    hello world server

---
+++ helloWorld.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 8000
+
+// http server
+const server = http.createServer((req, res) => {
+  // response header & content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+
+  // sending response body
+  res.end('Hello World\n')
+})
+
+// message on console
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
