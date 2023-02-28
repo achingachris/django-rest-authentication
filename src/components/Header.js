57159ca0dbdefcdb764712c0d39f8e354f475179
achingachris <achinga.chris@gmail.com>
2023-02-28 23:34:00 +0300

    updated-components

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className='masthead'>
+      <div className='container'>
+        <div className='masthead-subheading'>Welcome To Our Studio!</div>
+        <div className='masthead-heading text-uppercase'>
+          It is Nice To Meet You
+        </div>
+        <a className='btn btn-primary btn-xl text-uppercase' href='#services'>
+          Tell Me More
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
